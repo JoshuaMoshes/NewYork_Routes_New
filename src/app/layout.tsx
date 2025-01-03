e72b376044e6d13c0753184c2880771bea6b395f
Joshua Moshes <joshuam6060@gmail.com>
2025-01-03 17:03:20 -0500

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
+
 import "jsvectormap/dist/css/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
+import Script from "next/script"; // Import the Script component
 
 export default function RootLayout({
   children,
@@ -22,6 +24,22 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DMDY79XLBH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-DMDY79XLBH');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning={true}>
         {loading ? <Loader /> : children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,17 @@ import DefaultLayout from "@/components/Layouts/DefaultLaout";
 import React from "react";
 
 export const metadata: Metadata = {
-  title:
-    "Congestion Pricing Tracker | Benjamin and Joshua Moshes",
-  description: "This project is run by Joshua Moshes and Benjamin Moshes, under the supervision of Brown University Professor Emily Oster",
-  authors: [{name: "Joshua Moshes", url:"CongestionPricingTracker.com"}],
-  icons: [
-    { rel: "icon", url: "/images/favicon.ico" },
-    // { rel: "icon", url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    // { rel: "icon", url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    // { rel: "apple-touch-icon", url: "/icons/apple-touch-icon.png", sizes: "180x180" },
-    // Add more as needed
-  ],
+  title: "Congestion Pricing Tracker | Benjamin and Joshua Moshes",
+  description:
+    "This project is run by Joshua Moshes and Benjamin Moshes, under the supervision of Brown University Professor Emily Oster",
+  authors: [{ name: "Joshua Moshes", url: "https://CongestionPricingTracker.com" }],
+  icons: [{ rel: "icon", url: "/images/favicon.ico" }],
 };
 
 export default function Home() {
   return (
     <>
-      <ECommerce>
-        {/* <ECommerce /> */}
-        {/* <ChartOne /> */}
-      </ECommerce>
-      
+      <ECommerce />
     </>
   );
 }
